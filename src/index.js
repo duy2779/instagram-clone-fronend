81c77ae1a1931cc31a1c4b55db1453786a687495
duy <jultqd99@gmail.com>
2021-07-21 20:14:36 +0700

    deploy ready 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './app.css';
+import './styles.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
